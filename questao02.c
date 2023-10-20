4e3b33ca0596469e061767a507b956d28363b1a7
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-10-20 15:26:49 -0300

    Update questao02.c

--- questao02.c
+++ questao02.c
@@ -25,7 +25,7 @@ int main() {
     for (int i = 0; i < 1000; i++) {
         if (vetor[i] == numero) {
             printf("%d ", i);
-            encontrado ++ 1;
+            encontrado ++;
         }
     }
 
